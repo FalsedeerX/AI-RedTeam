721ac7326f86997b51276fe9b7fb2e8ec983dd79
Falsedeer <KLORE@AbyssWalker.onion>
2026-01-25 16:50:25 -0500

    Create definition for table Users and Projects

--- backend/app/db/models/projects.py
+++ backend/app/db/models/projects.py
@@ -1,2 +1,11 @@
+from app.db.base import Base
+
+
+class Projects(Base):
+    __tablename__ = "projects"
+    __table_args__ = {"schema": "app"}
+
+
+
 if __name__ == "__main__":
     pass
--- backend/app/db/models/users.py
+++ backend/app/db/models/users.py
@@ -1,3 +1,23 @@
+from __future__ import annotations
+from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Boolean, DateTime, text, Text, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.db.base import Base
+
+
+class Users(Base):
+    __tablename__ = "users"
+    __table_args__ = {"schema": "app"}
+
+    id: Mapped[str] = mapped_column(UUID(as_uuid=True), primary_key=True, server_default=text("app.gen_random_uuid()"))
+    email: Mapped[str] = mapped_column(Text, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(Text, nullable=False)
+    is_admin: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default=text("false"))
+    is_verified: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default=text("false")))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=False), nullable=False, server_default=func.now())
+    projects: Mapped[list["Projects"]] = relationship("Projects", back_populates="owner", cascade="all, delete-orphan", passive_deletes=True)
+
 
 if __name__ == "__main__":
     pass
